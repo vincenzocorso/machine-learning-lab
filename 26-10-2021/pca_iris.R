1588dd21fc3523e2e5622601d389fce0f6d28829
vincenzocorso <vincenzocorso99@gmail.com>
2021-10-31 16:08:36 +0100

    Update assignment

--- 26-10-2021/pca_iris.R
+++ 26-10-2021/pca_iris.R
@@ -7,31 +7,58 @@ data("iris")
 # Select the PCA input dataset
 iris.active = iris[, 1:4]
 
-iris.pca <- PCA(iris.active, scale.unit = TRUE, ncp = 5, graph = TRUE)
-
-iris.eig.val <- get_eigenvalue(iris.pca)
-iris.eig.val
+iris.pca <- PCA(iris.active, graph = TRUE)
 
 # How many components are necessary to explain the variance of the data?
-# R: Two component (the first) -> 95.8%%
+# Get the eigenvalues
+iris.eig.val <- get_eigenvalue(iris.pca)
+iris.eig.val
+# Draw some graphs
 fviz_eig(iris.pca, addlabels = TRUE, ylim = c(0, 100))
+# R: The first two components explain 95.8% of variance
 
-# Determine how many components need to be selected to explain most of the variance of the species
-var <- get_pca_var(iris.pca)
-head(var$coord, 4)
+# Plot all individuals
+iris.ind <- get_pca_ind(iris.pca)
+iris.ind
+fviz_pca_ind(iris.pca,
+  axes = c(1,2),
+  geom = c("point", "text"),
+  col.ind = "cos2",
+  ggtheme = theme_minimal()
+)
 
 # Filter data by plotting only the top-5 individuals
-ind <- get_pca_ind(iris.pca)
-ind
 fviz_pca_ind(iris.pca,
   select.ind = list(cos2 = 5),
   ggtheme = theme_minimal()
 )
 
-# Filter data by plotting the 2 top-quality variables
+# Plot all variables
 iris.var <- get_pca_var(iris.pca)
 iris.var
 fviz_pca_var(iris.pca,
+  axes = c(1,2),
+  geom = c("arrow", "text"),
+  ggtheme = theme_minimal()
+)
+# Petal.Width e Petal.Length are positive related
+
+# Filter data by plotting the 2 top-quality variables
+fviz_pca_var(iris.pca,
   select.var = list(cos2 = 2),
+  ggtheme = theme_minimal()
+)
+
+# Plot top 3 individuals and variables
+fviz_pca_biplot(
+  iris.pca,
+  axes = c(1,2),
+  select.ind = list(cos2 = 3),
+  select.var = list(cos2 = 3),
+  geom.ind = c("point", "text"),
+  geom.var = c("arrow", "text"),
+  col.ind = iris$Species,
+  palette = c("red", "blue", "green"),
+  legend.title = "Species",
   ggtheme = theme_minimal()
 )
